7719230d3c2d2141bde41aa8da19520ae7816236
Fort <65764413+Fortinenta@users.noreply.github.com>
2020-12-14 20:07:10 +0700

    1st

---
+++ app/Http/Controllers/shController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class shController extends Controller
+{
+    //
+    public function try(){
+        return redirect('https://docs.google.com/document/d/1pRiHQeujNk_XLhTO_W_ZDEbtB6821zXfVh0F6FlUxoI/edit');
+    }
+}
